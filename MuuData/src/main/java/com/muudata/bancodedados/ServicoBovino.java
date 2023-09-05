f5b3dcfe729396731d9be5b54e804749f2b7f62b
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-09-05 08:46:28 -0300

    Update ServicoBovino.java

--- MuuData/src/main/java/com/muudata/bancodedados/ServicoBovino.java
+++ MuuData/src/main/java/com/muudata/bancodedados/ServicoBovino.java
@@ -4,7 +4,13 @@
  */
 package com.muudata.bancodedados;
 
+import com.muudata.bancodedados.conexao.Conexao;
 import com.muudata.entites.Bovino;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,8 +19,29 @@ import java.util.List;
  */
 public class ServicoBovino {
     public List<Bovino> consultaBovino() {
-        //TODO
-        return null;
+        List<Bovino> retorno = new ArrayList<Bovino>();
+        
+        try {
+            Connection c = new Conexao().obterConexao();
+            String sql = "SELECT * FROM arthur_ribeiro.bovino";
+            PreparedStatement consulta = c.prepareStatement(sql);
+            ResultSet resultado = consulta.executeQuery();
+            while(resultado.next()) {
+                Bovino atual = new Bovino();
+                atual.setBrinco(resultado.getInt("brinco"));
+                atual.setIdentificador(resultado.getInt("identificador"));
+                atual.setNascimento(resultado.getDate("nascimento"));
+                atual.setNome(resultado.getString("nome"));
+                atual.setPeso(resultado.getShort("peso"));
+                atual.setSexo(resultado.getBoolean("sexo"));
+                atual.setRaca(resultado.getString("raca"));
+                retorno.add(atual);
+            }
+        } catch (SQLException ex) {
+            System.err.println("SQL ERROR "+ ex.getMessage());
+        }
+        
+        return retorno;
     }
     public void registraBovino(Bovino bovino) {
         //TODO  
