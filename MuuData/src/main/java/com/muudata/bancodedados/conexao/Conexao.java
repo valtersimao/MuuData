e9213690bdb2e37163e465bfb8a07feda62bc97e
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-09-05 08:17:02 -0300

    Created Bovino and updated conexao

--- MuuData/src/main/java/com/muudata/bancodedados/conexao/Conexao.java
+++ MuuData/src/main/java/com/muudata/bancodedados/conexao/Conexao.java
@@ -4,10 +4,31 @@
  */
 package com.muudata.bancodedados.conexao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author 0068943
  */
 public class Conexao {
+    private static Connection ifConexoes;
     
+    public static Connection obterConexao() {
+        try {
+            String url = "jdbc:postgresql://10.90.24.56:5432/aula";
+            String usuario = "aula";
+            String senha = "aula";
+
+            Connection con = DriverManager.getConnection(url, usuario, senha);
+            return con;
+        } catch (java.sql.SQLException ex) {
+            System.out.println("Erro ao conectar no banco: "+ex.getMessage());
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
 }
