4eca0addf1b19acd492883d2e0eaa26d45eee6d3
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-09-05 07:53:33 -0300

    Create ServicoBovino.java

---
+++ MuuData/src/main/java/com/muudata/bancodedados/ServicoBovino.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.muudata.bancodedados;
+
+/**
+ *
+ * @author 0068943
+ */
+public class ServicoBovino {
+    
+}
