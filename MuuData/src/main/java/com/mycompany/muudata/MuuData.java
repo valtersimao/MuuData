187e28020456c98a1c3f5ae2e510ec29e224f98b
Valter Simão <103610549+valtersimao@users.noreply.github.com>
2023-09-05 07:46:06 -0300

    Created Java Project

---
+++ MuuData/src/main/java/com/mycompany/muudata/MuuData.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.muudata;
+
+/**
+ *
+ * @author 0068967
+ */
+public class MuuData {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
