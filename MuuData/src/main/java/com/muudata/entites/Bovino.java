1f68db6df98c5b44953b04b87d8cbc0e68c08358
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-09-05 08:29:51 -0300

    Update Bovino.java

--- MuuData/src/main/java/com/muudata/entites/Bovino.java
+++ MuuData/src/main/java/com/muudata/entites/Bovino.java
@@ -4,10 +4,75 @@
  */
 package com.muudata.entites;
 
+import java.sql.Date;
+
 /**
  *
  * @author 0068943
  */
 public class Bovino {
+    private int brinco;
+    private int identificador;
+    private String nome;
+    private String raca;
+    private boolean sexo;
+    private Date nascimento;
+    private int peso;
+
+    public int getBrinco() {
+        return brinco;
+    }
+
+    public void setBrinco(int brinco) {
+        this.brinco = brinco;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public boolean isSexo() {
+        return sexo;
+    }
+
+    public void setSexo(boolean sexo) {
+        this.sexo = sexo;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
     
 }
