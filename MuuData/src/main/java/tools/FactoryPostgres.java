f737f1859207889117d81c1ee852e9e3186dbbff
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-10-10 07:58:20 -0300

    Create FactoryPostgres.java

---
+++ MuuData/src/main/java/tools/FactoryPostgres.java
@@ -0,0 +1,25 @@
+package tools;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author 0068943
+ */
+public class FactoryPostgres {
+    private static Connection conexaoPG;
+    
+    public static Connection getConexaoPostgres() {
+        if(conexaoPG == null) {
+            try {
+                conexaoPG = DriverManager.getConnection("jdbc:postgresql://200.18.128.54:5432/muudata", "muudata", "roca123");
+            } catch (SQLException ex) {
+                System.err.println("Houve um erro na conexão!");
+                return null;
+            }
+        }
+        return conexaoPG;
+    }
+}
