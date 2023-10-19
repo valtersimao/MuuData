545c8fc751ef53ff8ed9c8fb8a32252933f25e83
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-10-19 08:23:00 -0300

    Ajustando BovinoDAO para o padrão

--- MuuData/src/main/java/model/dao/BovinoDAO.java
+++ MuuData/src/main/java/model/dao/BovinoDAO.java
@@ -8,8 +8,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
 import model.Bovino;
 import tools.FactoryPostgres;
 
@@ -17,9 +17,88 @@ import tools.FactoryPostgres;
  *
  * @author 0068943
  */
-public class BovinoDAO {
-    public static List<Bovino> consultaBovino() {
-        List<Bovino> retorno = new ArrayList<Bovino>();
+public class BovinoDAO implements DAO{
+    
+    private Connection c;
+    //Caso o bovino seja cadastrado sem nenhum problema a função retorna verdadeiro, caso contrário falso
+    public static boolean registraBovino(Bovino bovino) {
+          
+        try {
+            Connection c = FactoryPostgres.getConexaoPostgres();
+            String sql = "INSERT INTO muudata.bovino(brinco, nome, peso, sexo, raca) "
+            + "VALUES (?,?,?,?,?);";
+            PreparedStatement trans;
+            trans = c.prepareStatement(sql);
+            trans.setInt(1, bovino.getBrinco());
+            trans.setString(2, bovino.getNome());
+            trans.setInt(3, bovino.getPeso());
+            trans.setBoolean(4, bovino.isSexo());
+            trans.setString(5, bovino.getRaca());
+            trans.executeQuery();
+            c.close();
+            
+            return true;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
+          
+    }
+
+    public BovinoDAO() {
+         c = FactoryPostgres.getConexaoPostgres();
+    }
+
+    @Override
+    public boolean insert(Object entite) {
+        Bovino bovino = (Bovino) entite;
+        
+        String sql = "INSERT INTO muudata.bovino(brinco, nome, peso, sexo, raca) "
+            + "VALUES (?,?,?,?,?);";
+        
+        try(PreparedStatement trans = this.c.prepareStatement(sql, 
+                Statement.RETURN_GENERATED_KEYS)){
+            
+            trans.setInt(1, bovino.getBrinco());
+            trans.setString(2, bovino.getNome());
+            trans.setInt(3, bovino.getPeso());
+            trans.setBoolean(4, bovino.isSexo());
+            trans.setString(5, bovino.getRaca());
+            trans.executeQuery();
+            
+            ResultSet resultSet = trans.getGeneratedKeys();
+            
+            if(resultSet.next()) {
+                bovino.setIdentificador(resultSet.getInt("id"));
+                
+                return true;
+            }else {
+               return false; 
+            }
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public boolean update(Object entite) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean delete(long id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object getById(long id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<Object> getAll() {
+        ArrayList<Object> retorno = new ArrayList<>();
         
         try {
             Connection c = FactoryPostgres.getConexaoPostgres();
@@ -37,36 +116,15 @@ public class BovinoDAO {
                 atual.setRaca(resultado.getString("raca"));
                 retorno.add(atual);
             }
-            c.close();
         } catch (SQLException ex) {
             System.err.println("SQL ERROR "+ ex.getMessage());
         }
         
         return retorno;
     }
-    
-    //Caso o bovino seja cadastrado sem nenhum problema a função retorna verdadeiro, caso contrário falso
-    public static boolean registraBovino(Bovino bovino) {
-          
-        try {
-            Connection c = FactoryPostgres.getConexaoPostgres();
-            String sql = "INSERT INTO muudata.bovino(brinco, nome, peso, sexo, raca) "
-            + "VALUES (?,?,?,?,?);";
-            PreparedStatement insercao;
-            insercao = c.prepareStatement(sql);
-            insercao.setInt(1, bovino.getBrinco());
-            insercao.setString(2, bovino.getNome());
-            insercao.setInt(3, bovino.getPeso());
-            insercao.setBoolean(4, bovino.isSexo());
-            insercao.setString(5, bovino.getRaca());
-            insercao.executeQuery();
-            c.close();
-            
-            return true;
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-            return false;
-        }
-          
+
+    @Override
+    public ArrayList<Object> getWithFilter(Object filter) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
--- MuuData/src/main/java/model/dao/DAO.java
+++ MuuData/src/main/java/model/dao/DAO.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
  * @author arthur
  */
 public interface DAO {
-    public Object insert(Object entite);
-    public Object update(Object entite);
+    public boolean insert(Object entite);
+    public boolean update(Object entite);
     public boolean delete(long id);
     public Object getById(long id);
     public ArrayList<Object> getAll();
