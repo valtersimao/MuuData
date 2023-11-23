fa028cf9ccf4e841ae609fd78da8deb4e44966f0
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-11-23 12:07:09 -0300

    Create Vacina.java

---
+++ MuuData/src/main/java/model/Vacina.java
@@ -0,0 +1,84 @@
+
+package model;
+
+import java.time.LocalTime;
+
+public class Vacina {
+    private int idVacina;
+    private String nome;
+    private short dosesRecomendadas;
+    
+    private int idHistorico;
+    private LocalTime dataEvento;
+    private short dose;
+
+    public Vacina(int idVacina, String nome, short dosesRecomendadas) {
+        this.idVacina = idVacina;
+        this.nome = nome;
+        this.dosesRecomendadas = dosesRecomendadas;
+    }
+
+    public Vacina(String nome, short dosesRecomendadas) {
+        this.nome = nome;
+        this.dosesRecomendadas = dosesRecomendadas;
+    }
+
+    public Vacina(int idVacina, String nome, short dosesRecomendadas, int idHistorico, LocalTime dataEvento, short dose) {
+        this.idVacina = idVacina;
+        this.nome = nome;
+        this.dosesRecomendadas = dosesRecomendadas;
+        this.idHistorico = idHistorico;
+        this.dataEvento = dataEvento;
+        this.dose = dose;
+    }
+
+    public int getIdVacina() {
+        return idVacina;
+    }
+
+    public void setIdVacina(int idVacina) {
+        this.idVacina = idVacina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public short getDosesRecomendadas() {
+        return dosesRecomendadas;
+    }
+
+    public void setDosesRecomendadas(short dosesRecomendadas) {
+        this.dosesRecomendadas = dosesRecomendadas;
+    }
+
+    public int getIdHistorico() {
+        return idHistorico;
+    }
+
+    public void setIdHistorico(int idHistorico) {
+        this.idHistorico = idHistorico;
+    }
+
+    public LocalTime getDataEvento() {
+        return dataEvento;
+    }
+
+    public void setDataEvento(LocalTime dataEvento) {
+        this.dataEvento = dataEvento;
+    }
+
+    public short getDose() {
+        return dose;
+    }
+
+    public void setDose(short dose) {
+        this.dose = dose;
+    }
+    
+    
+}
