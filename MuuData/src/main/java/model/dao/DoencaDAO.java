10849ab92a04c3bf5703d0380c56f81e82dc34f6
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-11-28 08:40:18 -0300

    Update DoencaDAO.java

--- MuuData/src/main/java/model/dao/DoencaDAO.java
+++ MuuData/src/main/java/model/dao/DoencaDAO.java
@@ -4,10 +4,42 @@
  */
 package model.dao;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author 0068943
  */
-public class DoencaDAO {
+public class DoencaDAO implements DAO{
+
+    @Override
+    public boolean insert(Object entite) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean update(Object entite) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean delete(long id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object getById(long id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<Object> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<Object> getWithFilter(Object filter) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
