8b85b170cd9274a1ccc53a59db14b96746b040cf
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-11-29 09:55:36 -0300

    Update FazendaControl.java

--- MuuData/src/main/java/control/FazendaControl.java
+++ MuuData/src/main/java/control/FazendaControl.java
@@ -13,6 +13,8 @@ import tools.PasswordService;
  * @author 0068943
  */
 public class FazendaControl {
+    public static final int NO_PROBLEM = 0, USER_NOT_FOUND = 1, USER_EXISTS = 2, INVALID_PASSWORD = 3, ANOTHER_ERROR = 4;
+            
     private final FazendaDAO dao;
     private final PasswordService passwordService;
     
@@ -25,31 +27,37 @@ public class FazendaControl {
         return dao.getByName(name) != null;
     }
     
-    public Fazenda logIn(String name, char[] senha) {
+    public int logIn(String name, char[] senha) {
         return logIn(name, new String(senha));
     }
     
-    public Fazenda logIn(String name, String senha) {
+    public int logIn(String name, String senha) {
         Fazenda fazenda = dao.getByName(name);
+        if(fazenda == null) {
+            return USER_NOT_FOUND;
+        }
         
         if(passwordService.validatePassword(fazenda.getSenha(), senha)){
-            return fazenda;
+            return NO_PROBLEM;
         }else {
-            return null;
+            return INVALID_PASSWORD;
         }
     }
     
-    public Fazenda signIn(String password, String name, String email) {
+    public int signIn(String password, String name, String email) {
         if(!doesNameExists(name)){
             password = passwordService.hashPassword(password);
-            Fazenda fazenda = new Fazenda(name, email, email);
+            Fazenda fazenda = new Fazenda(name, email, password);
             if(dao.insert(fazenda)){
-                return fazenda;
+                return NO_PROBLEM;
+            }else{
+                return ANOTHER_ERROR;
             }
+        }else {
+            return USER_EXISTS;
         }
-        return null;
     }
-    public Fazenda signIn(char[] password, String name, String email){
+    public int signIn(char[] password, String name, String email){
         return signIn(new String(password), name, email);
     }
 }
