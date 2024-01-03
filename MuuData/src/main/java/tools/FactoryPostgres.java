4b5e5b339d9243bec697cc496c0b3d53eb18f04f
Valter Simão <103610549+valtersimao@users.noreply.github.com>
2024-01-03 18:35:53 -0300

    Update FactoryPostgres.java

--- MuuData/src/main/java/tools/FactoryPostgres.java
+++ MuuData/src/main/java/tools/FactoryPostgres.java
@@ -5,12 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class FactoryPostgres {
+
     private static Connection conexaoPG;
-    
+
     public static Connection getConexaoPostgres() {
-        if(conexaoPG == null) {
+        if (conexaoPG == null) {
             try {
-                conexaoPG = DriverManager.getConnection("jdbc:postgresql://10.90.24.54:5432/muudata", "muudata", "roca123");
+                conexaoPG = DriverManager.getConnection("jdbc:postgresql://200.18.128.54:5432/muudata", "muudata", "roca123"); //CONECTAR DE FORA DA ESCOLA
+                //conexaoPG = DriverManager.getConnection("jdbc:postgresql://10.90.24.54:5432/muudata", "muudata", "roca123"); //CONECTAR DA ESCOLA
             } catch (SQLException ex) {
                 System.err.println("Houve um erro na conexão!");
                 return null;
